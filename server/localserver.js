59ebc16570ec54452888dc75e08a01e323322073
Wesley Hinchman <95438151+weshinchman@users.noreply.github.com>
2022-07-07 09:30:03 +0200

    Update localserver.js

--- server/localserver.js
+++ server/localserver.js
@@ -1,20 +1,14 @@
 const { app, router } = require("./app.js");
+const config = require('./config');
 app.use(router);
 const server = require("http").createServer(app);
-if (process.env.AZURE_IOT_HUB_EVENT_HUB_CONNECTION_STRING) {
+
+if (config.azure.azure_conn_string) {
     require("./RealTimeApi.js").createSocketIOServer(server);
 }
 
 const PORT = process.env.PORT || 9000;
 
 async function start() {
-    try {
-        server.listen(PORT, () => {
-            console.log(`http://localhost:${PORT}`);
-        });
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-start();
+    try { server.listen(PORT, () => { console.log(`localhost: ${PORT}`); }); } catch (error) { console.log(error); }
+} start();
